bcf78a6607649258dfa5acfea627fec9b8254eeb
Кирилл <myakiishevkirill@gmail.com>
2022-03-21 21:25:59 +1000

    added division

--- calculator.cpp
+++ calculator.cpp
@@ -11,7 +11,8 @@ int main()
 	cout<<"Choose a math operation"<<endl<<
 	    "1-Addition"<<endl<<
 	    "2-Subtraction"<<endl<<
-	    "3-Multiplication"<<endl;
+	    "3-Multiplication"<<endl<<
+        "4-Division"<<endl;
 	    
 	cin>>selection;
 	
@@ -24,6 +25,8 @@ int main()
 		cout << a-b << endl;
 	case 3:
 	    cout << a*b < <endl;
+	case 4:
+	    cout << a/b << endl;
 		break;
 	default:
 		cout<<"Error! There is no such operation!"<<endl;
